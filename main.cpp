ef91cec64c2ebc10a22c9255d6d0f5c0202d347b
Amanda Bee <amanda1224.jyb@gmail.com>
2025-09-21 22:42:52 +0800

    ui added and initial value

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include<iostream>
 #include<iomanip>
 
+#include"staff.cpp"
+#include"ui.cpp"
+
 using namespace std;
+
+void printWelcome();
+int availableD(Double room[]);
+int availableP(Presidential room[]);
+int availableV(Villa room[]);
+
+int main(){
+	Customer *head = NULL;
+	int choice;
+	// Declaring 3 types of rooms, each with 10 space to represent 10 rooms for storing customer.
+	Double dRoom[10];
+	Presidential pRoom[10];
+	Villa vRoom[10];
+	// Initialize the Staff members in the system (3 staffs)
+	Staff s[3] = {Staff("Amanda", 1234, 18, "BOSS", "imBoss"), Staff("Abu", 2345, 35, "Manager", "imManager"), Staff("Ahmad", 3456, 25, "Receptionist", "imReceptionist")};
+}
 
 // Function to check how many double rooms are available
 int availableD(Double room[]){
