21beb3bf9acc9cc12333f84a641e8a2ec4b41c03
Ammar Afzan <125238102+zarkmuckerber@users.noreply.github.com>
2025-09-22 18:41:36 +0800

    Implement hotel room preference selection logic

--- get_hotel_room_preferences.cpp
+++ get_hotel_room_preferences.cpp
@@ -25,3 +25,123 @@ void roomBooked(Customer &c, Double d[], Presidential p[], Villa v[]);
 int calcD(Customer &c, Double d[]);
 int calcP(Customer &c, Presidential p[]);
 int calcV(Customer &c, Villa v[]);
+
+void get_hotel_room_preferences(Customer &c, Double d[], Presidential p[], Villa v[]){
+    int roomtype, numNights, numRooms, choice ;
+    string roomtypeCopy, startDate;
+
+    do{
+        booking_menu(c, d, p, v);
+        cout<<endl<<"\n                                                                     Please enter your choice(1-3) : ";
+        cin>>roomtype;
+
+        if(roomtype > 3 || roomtype < 1){
+            re_enter();
+            choice = 2;
+            continue ;
+        }
+
+        cout<<"\n                                   Please enter number of rooms that you needed for this room type : ";
+        cin>>numRooms;
+
+        
+        cout<<"\n                                          Please enter number of night(s) you are planning to stay : ";
+        cin>>numNights;
+
+        cout<<"\n                                                        Please enter your check-in date [DD-MM-YY] : ";
+        cin>>startDate;
+
+        switch (roomtype){
+            case 1:
+                roomtypeCopy = "Double Room";
+                if (availableD(d) == 0)
+                {
+                    noRoom();
+                    continue;
+                }
+                else if (availableD(d) < numRooms)
+                {
+                    noEnough();
+                    continue;
+                }
+                else{
+                    for (int i = 0; i < numRooms; ++i){
+                        for (int j = 0; j < 10; ++j)
+                        {
+                            if (d[j].customer == "")
+                            {
+                                d[j].customer = c.ic;
+                                d[j].nights = numNights;
+                                d[j].startDate = startDate;
+                                break;
+                            }
+                        }
+                    }
+                }
+                break;
+                
+            case 2:
+                roomtypeCopy = "Presidential Room";
+                if (availableP(p) == 0)
+                {
+                    noRoom();
+                    continue;
+                }
+                else if (availableP(p) < numRooms)
+                {
+                    noEnough();
+                    continue;
+                }
+                else{
+                    for (int i = 0; i < numRooms; ++i){
+                        for (int j = 0; j < 10; ++j)
+                        {
+                            if (p[j].customer == "")
+                            {
+                                p[j].customer = c.ic;
+                                p[j].nights = numNights;
+                                p[j].startDate = startDate;
+                                break;
+                            }
+                        }
+                    }
+                }
+                break;
+
+            case 3:
+                roomtypeCopy = "Villa";
+                if (availableV(v) == 0)
+                {
+                    noRoom();
+                    continue;
+                }
+                else if (availableV(v) < numRooms)
+                {
+                    noEnough();
+                    continue;
+                }
+                else{
+                    for (int i = 0; i < numRooms; ++i){
+                        for (int j = 0; j < 10; ++j)
+                        {
+                            if (v[j].customer == "")
+                            {
+                                v[j].customer = c.ic;
+                                v[j].nights = numNights;
+                                v[j].startDate = startDate;
+                                break;
+                            }
+                        }
+                    }
+                }
+                break;
+        }
+        
+        printThanks();
+        getChoice();
+        cin>>choice;
+
+    } while (choice == 2);
+
+    roomBooked(c, d, p, v);
+}
