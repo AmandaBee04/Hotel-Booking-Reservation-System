9ddab1a44c10ba71fac055a0137276317d0ce5b4
Desmond0411 <desmondglj@gmail.com>
2025-09-23 14:34:43 +0800

    Update classRoom.cpp

--- classRoom.cpp
+++ classRoom.cpp
@@ -9,7 +9,55 @@ class Villa;
 // This is the class of Double room, which is the parent class of Presidential and Villa.
 class Double
 {
-    
+    protected:
+        int roomNum;
+        string customer;
+        int nights;
+        float price;
+        char choice;
+        string startDate;
+
+    public:
+        Double()
+        {
+            price = 10000;
+            nights = 0;
+            customer = "";
+        }
+
+        void setRoomNum(int rn)
+        {   
+            roomNum = rn;
+        }
+
+        void changeDetails()
+        {
+            customer = "";
+        }
+
+        int get_roomNum()
+        {   
+            return roomNum;
+        }
+
+        void reset(){
+            customer = "";
+            nights = 0;
+            startDate = "";
+        }
+
+        string getCustomer()
+        {
+            return customer;
+        }
+
+        int getNights(){
+            return nights;
+        }
+
+        float getPrice(){
+            return price;
+        }
 
     
 };
